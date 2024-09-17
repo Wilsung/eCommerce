7ecc07c5abdfb382f4a61161a63d3cf81f5f4c54
Wilson <wou123@yahoo.com>
2024-09-17 12:46:42 -0700

    server update

--- src/server.ts
+++ src/server.ts
@@ -8,67 +8,27 @@ dotenv.config({
 })
 
 import express from 'express'
+// import { getPayloadClient } from './payload/getPayload'
+import payload from 'payload'
 
-import { getPayloadClient } from './payload/getPayload'
-
-// import payload from 'payload'
-// import { seed } from './payload/seed'
+import { seed } from './payload/seed'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// const start = async (): Promise<void> => {
-//   await payload.init({
-//     secret: process.env.PAYLOAD_SECRET || '',
-//     express: app,
-//     onInit: () => {
-//       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-//     },
-//   })
-
-//   if (process.env.PAYLOAD_SEED === 'true') {
-//     await seed(payload)
-//     process.exit()
-//   }
-
-//   if (process.env.NEXT_BUILD) {
-//     app.listen(PORT, async () => {
-//       payload.logger.info(`Next.js is now building...`)
-//       // @ts-expect-error
-//       await nextBuild(path.join(__dirname, '../'))
-//       process.exit()
-//     })
-
-//     return
-//   }
-
-//   const nextApp = next({
-//     dev: process.env.NODE_ENV !== 'production',
-//   })
-
-//   const nextHandler = nextApp.getRequestHandler()
-
-//   app.use((req, res) => nextHandler(req, res))
-
-//   nextApp.prepare().then(() => {
-//     payload.logger.info('Starting Next.js...')
-
-//     app.listen(PORT, async () => {
-//       payload.logger.info(`Next.js App URL: ${process.env.PAYLOAD_PUBLIC_SERVER_URL}`)
-//     })
-//   })
-// }
-
 const start = async (): Promise<void> => {
-  const payload = await getPayloadClient({
-    initOptions: {
-      express: app,
-      onInit: async newPayload => {
-        newPayload.logger.info(`Payload Admin URL: ${newPayload.getAdminURL()}`)
-      },
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET || '',
+    express: app,
+    onInit: () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
     },
-    seed: process.env.PAYLOAD_PUBLIC_SEED === 'true',
   })
+
+  if (process.env.PAYLOAD_SEED === 'true') {
+    await seed(payload)
+    process.exit()
+  }
 
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
@@ -90,12 +50,51 @@ const start = async (): Promise<void> => {
   app.use((req, res) => nextHandler(req, res))
 
   nextApp.prepare().then(() => {
-    payload.logger.info('Next.js started')
+    payload.logger.info('Starting Next.js...')
 
     app.listen(PORT, async () => {
       payload.logger.info(`Next.js App URL: ${process.env.PAYLOAD_PUBLIC_SERVER_URL}`)
     })
   })
 }
+
+// const start = async (): Promise<void> => {
+//   const payload = await getPayloadClient({
+//     initOptions: {
+//       express: app,
+//       onInit: async newPayload => {
+//         newPayload.logger.info(`Payload Admin URL: ${newPayload.getAdminURL()}`)
+//       },
+//     },
+//     seed: process.env.PAYLOAD_PUBLIC_SEED === 'true',
+//   })
+
+//   if (process.env.NEXT_BUILD) {
+//     app.listen(PORT, async () => {
+//       payload.logger.info(`Next.js is now building...`)
+//       // @ts-expect-error
+//       await nextBuild(path.join(__dirname, '../'))
+//       process.exit()
+//     })
+
+//     return
+//   }
+
+//   const nextApp = next({
+//     dev: process.env.NODE_ENV !== 'production',
+//   })
+
+//   const nextHandler = nextApp.getRequestHandler()
+
+//   app.use((req, res) => nextHandler(req, res))
+
+//   nextApp.prepare().then(() => {
+//     payload.logger.info('Next.js started')
+
+//     app.listen(PORT, async () => {
+//       payload.logger.info(`Next.js App URL: ${process.env.PAYLOAD_PUBLIC_SERVER_URL}`)
+//     })
+//   })
+// }
 
 start()

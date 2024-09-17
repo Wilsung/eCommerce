52e784ad18032d5e98fb52c9a1c7d95fa128a4b6
Wilson <wou123@yahoo.com>
2024-09-17 13:40:06 -0700

    removed getpayload

--- src/payload/getPayload.ts
+++ src/payload/getPayload.ts
@@ -1,59 +1,59 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import dotenv from 'dotenv'
-import path from 'path'
-import type { Payload } from 'payload'
-import payload from 'payload'
-import type { InitOptions } from 'payload/config'
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import dotenv from 'dotenv'
+// import path from 'path'
+// import type { Payload } from 'payload'
+// import payload from 'payload'
+// import type { InitOptions } from 'payload/config'
 
-import { seed as seedData } from './seed'
+// import { seed as seedData } from './seed'
 
-dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
-})
+// dotenv.config({
+//   path: path.resolve(__dirname, '../.env'),
+// })
 
-let cached = (global as any).payload
+// let cached = (global as any).payload
 
-if (!cached) {
-  cached = (global as any).payload = { client: null, promise: null }
-}
+// if (!cached) {
+//   cached = (global as any).payload = { client: null, promise: null }
+// }
 
-interface Args {
-  initOptions?: Partial<InitOptions>
-  seed?: boolean
-}
+// interface Args {
+//   initOptions?: Partial<InitOptions>
+//   seed?: boolean
+// }
 
-export const getPayloadClient = async ({ initOptions, seed }: Args = {}): Promise<Payload> => {
-  if (!process.env.MONGODB_URI) {
-    throw new Error('MONGODB_URI environment variable is missing')
-  }
+// export const getPayloadClient = async ({ initOptions, seed }: Args = {}): Promise<Payload> => {
+//   if (!process.env.MONGODB_URI) {
+//     throw new Error('MONGODB_URI environment variable is missing')
+//   }
 
-  if (!process.env.PAYLOAD_SECRET) {
-    throw new Error('PAYLOAD_SECRET environment variable is missing')
-  }
+//   if (!process.env.PAYLOAD_SECRET) {
+//     throw new Error('PAYLOAD_SECRET environment variable is missing')
+//   }
 
-  if (cached.client) {
-    return cached.client
-  }
+//   if (cached.client) {
+//     return cached.client
+//   }
 
-  if (!cached.promise) {
-    cached.promise = payload.init({
-      secret: process.env.PAYLOAD_SECRET,
-      local: initOptions?.express ? false : true,
-      ...(initOptions || {}),
-    })
-  }
+//   if (!cached.promise) {
+//     cached.promise = payload.init({
+//       secret: process.env.PAYLOAD_SECRET,
+//       local: initOptions?.express ? false : true,
+//       ...(initOptions || {}),
+//     })
+//   }
 
-  try {
-    process.env.PAYLOAD_DROP_DATABASE = seed ? 'true' : 'false'
-    cached.client = await cached.promise
+//   try {
+//     process.env.PAYLOAD_DROP_DATABASE = seed ? 'true' : 'false'
+//     cached.client = await cached.promise
 
-    if (seed) {
-      await seedData(payload)
-    }
-  } catch (e: unknown) {
-    cached.promise = null
-    throw e
-  }
+//     if (seed) {
+//       await seedData(payload)
+//     }
+//   } catch (e: unknown) {
+//     cached.promise = null
+//     throw e
+//   }
 
-  return cached.client
-}
+//   return cached.client
+// }

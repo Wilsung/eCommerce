b23de7f960d4ed2ee38b141820c9a55db9916e62
Wilson <wou123@yahoo.com>
2024-09-17 12:11:22 -0700

    next edit

--- next.config.js
+++ next.config.js
@@ -15,37 +15,45 @@ const nextConfig = {
     domains: [
       'localhost',
       process.env.NEXT_PUBLIC_SERVER_URL,
-      'avatars.githubusercontent.com',
-      'avatar.tobi.sh',
-      'cloudflare-ipfs.com',
-      'loremflickr.com',
-      'mongodb://atlas-sql-66db7a7f5ba2ec5abdf1302a-7toff.a.query.mongodb.net/test?ssl=true&authSource=admin',
+      // 'mongodb://atlas-sql-66db7a7f5ba2ec5abdf1302a-7toff.a.query.mongodb.net/test?ssl=true&authSource=admin',
     ]
       .filter(Boolean)
       .map(url => url.replace(/https?:\/\//, '')),
   },
   redirects,
   async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
+    const headers = []
+
+    // Prevent search engines from indexing the site if it is not live
+    // This is useful for staging environments before they are ready to go live
+    // To allow robots to crawl the site, use the `NEXT_PUBLIC_IS_LIVE` env variable
+    // You may want to also use this variable to conditionally render any tracking scripts
+    if (!process.env.NEXT_PUBLIC_IS_LIVE) {
+      headers.push({
         headers: [
           {
             key: 'X-Robots-Tag',
             value: 'noindex',
           },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
         ],
-      },
-    ]
+        source: '/:path*',
+      })
+    }
+
+    // Set the `Content-Security-Policy` header as a security measure to prevent XSS attacks
+    // It works by explicitly whitelisting trusted sources of content for your website
+    // This will block all inline scripts and styles except for those that are allowed
+    headers.push({
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value: ContentSecurityPolicy,
+        },
+      ],
+    })
+
+    return headers
   },
 }
 
